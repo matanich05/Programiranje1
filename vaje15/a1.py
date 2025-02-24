f9aee35fa58fcc1fab000afd783feeb4d365e46e
Matija <matija.matanic05@gmail.com>
2025-02-24 15:03:56 +0100

    Add files via upload

--- vaje15/a1.py
+++ vaje15/a1.py
@@ -1,15 +1,20 @@
+import math
+
 def a1(stevilo):
     vsota = 0
-    for n in range(1, stevilo):
+    for n in range(1, int(math.sqrt(stevilo)) + 1):
         if stevilo % n == 0:
             vsota += n
+            if n != 1 and n != stevilo // n:
+                vsota += stevilo // n
+    
     if vsota == stevilo:
-        return stevilo / 3
+        return stevilo // 3
     elif abs(vsota - stevilo) <= 2:
         return vsota // 7
     else:
         return vsota // 14
 
-#stevilo = 8589934592
-stevilo = int(input(""))
+stevilo = 8589934592
 print(a1(stevilo))
+
